de4ddf20f5d45f01a41888a31fd9c1070b163ee9
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-07-08 18:20:45 -0300

    Requisito 1 feito, ta dificil hein

--- script.js
+++ script.js
@@ -12,32 +12,44 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
+  const items = document.querySelector('.items');
   const section = document.createElement('section');
   section.className = 'item';
-
+  console.log(sku, name, image);
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  return section;
+  return items.appendChild(section);
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-window.onload = () => { };
+const urlApi = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const fetchItemPromise = async () => fetch(urlApi)
+  .then((response) => response.json()
+  .then((data) => {
+    data.results.forEach((item) => {
+      createProductItemElement(item);
+    });
+  }));
+
+window.onload = () => { 
+  fetchItemPromise();
+};
